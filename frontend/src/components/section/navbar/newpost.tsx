e8e8a230a64609b235a5215e8686c8a8b7ade0b7
Yanda Agil <yandaagil@gmail.com>
2023-09-19 12:57:42 +0700

    feat: add styling for home and auth page

---
+++ frontend/src/components/section/navbar/newpost.tsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from '@/components/ui/textarea'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Globe, Image, ChevronsUpDown, Check } from "lucide-react"
+
+const categories = [
+  {
+    value: "sport",
+    label: "Sport",
+  },
+  {
+    value: "business",
+    label: "Business",
+  },
+  {
+    value: "health",
+    label: "Health",
+  },
+  {
+    value: "fashion",
+    label: "Fashion",
+  },
+  {
+    value: "technology",
+    label: "Technology",
+  },
+]
+
+const NewPost = () => {
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="font-bold">New Post</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-4xl">
+        <DialogHeader>
+          <DialogTitle>New Post</DialogTitle>
+          <DialogDescription>
+            What&apos;s on your mind?
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="flex flex-col space-y-3">
+            <Label htmlFor="title">Title</Label>
+            <Input type="text" name="title" id="title" placeholder="Summarize your thoughts" />
+          </div>
+          <div className="flex flex-col space-y-3">
+            <Label htmlFor="content">Paragraph</Label>
+            <Textarea className="min-h-[200px]" placeholder="Write your mind here" />
+          </div>
+        </div>
+        <DialogFooter className="flex-col sm:justify-between">
+          <div className="flex justify-start gap-2">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant="outline" size="icon"><Image /></Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Add an image</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant="outline" size="icon"><Globe /></Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Who can see your post?</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-[200px] justify-between"
+                >
+                  {value
+                    ? categories.find((category) => category.value === value)?.label
+                    : "Select category..."}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] p-0">
+                <Command>
+                  <CommandInput placeholder="Search category..." />
+                  <CommandEmpty>No category found.</CommandEmpty>
+                  <CommandGroup>
+                    {categories.map((category) => (
+                      <CommandItem
+                        key={category.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue)
+                          setOpen(false)
+                        }}
+                      >
+                        <Check className={"mr-2 h-4 w-4 " + (value === category.value ? "opacity-100" : "opacity-0")} />
+                        {category.label}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </Command>
+              </PopoverContent>
+            </Popover>
+          </div>
+          <Button type="submit">Post</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default NewPost
